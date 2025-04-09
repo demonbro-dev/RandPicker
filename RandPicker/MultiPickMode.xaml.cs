22c2bd852f6f114c0a18808eae87f95e0c7bc873
demonbro-dev <kuroama@foxmail.com>
2025-04-10 02:05:12 +0800

    修改了MultiPickMode实现，以尽可能避免重复抽选

--- RandPicker/MultiPickMode.xaml.cs
+++ RandPicker/MultiPickMode.xaml.cs
@@ -3,11 +3,11 @@
 // MultiPickMode.xaml.cs : RandPicker 多抽选模式页面后台实现
 //
 //
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Collections.ObjectModel;
 using System.Windows.Threading;
-using System.Diagnostics;
 
 namespace RandPicker
 {
@@ -18,6 +18,8 @@ namespace RandPicker
         private bool _isRunning = false;
         private ObservableCollection<string> _results = new ObservableCollection<string>();
         private Random _random = new Random();
+        private HashSet<string> _pickedNames = new HashSet<string>();
+        private string _currentListName;
 
         public MultiPickMode(PickerLogic logic)
         {
@@ -25,14 +27,12 @@ namespace RandPicker
             _logic = logic;
             resultItemsControl.ItemsSource = _results;
             InitializeTimer();
+            _currentListName = _logic.CurrentList;
         }
 
         private void InitializeTimer()
         {
-            _timer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(25)
-            };
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(25) };
             _timer.Tick += (s, e) =>
             {
                 try
@@ -47,20 +47,38 @@ namespace RandPicker
                         return;
                     }
 
-                    // 避免抽选到多个相同名字
-                    // 复制当前列表避免修改原始数据
-                    var tempList = new List<string>(currentList);
-                    _results.Clear();
+                    string currentListName = _logic.CurrentList;
+                    if (_currentListName != currentListName)
+                    {
+                        _pickedNames.Clear();
+                        _currentListName = currentListName;
+                    }
 
-                    int requiredCount = Math.Min(count, tempList.Count);
+                    var availableNames = currentList.Where(name => !_pickedNames.Contains(name)).ToList();
+
+                    if (availableNames.Count < count)
+                    {
+                        _pickedNames.Clear();
+                        availableNames = new List<string>(currentList);
+                    }
+
+                    if (availableNames.Count == 0)
+                    {
+                        _pickedNames.Clear();
+                        availableNames = new List<string>(currentList);
+                    }
+
+                    _results.Clear();
+                    int requiredCount = Math.Min(count, availableNames.Count);
+                    var tempList = new List<string>(availableNames);
+
                     for (int i = 0; i < requiredCount; i++)
                     {
                         int index = _random.Next(tempList.Count);
                         _results.Add(tempList[index]);
-                        tempList.RemoveAt(index); // 移除已选名字
+                        tempList.RemoveAt(index);
                     }
 
-                    // 补充提示（如果需求数量超过列表长度）
                     if (count > requiredCount)
                     {
                         for (int i = requiredCount; i < count; i++)
@@ -75,13 +93,20 @@ namespace RandPicker
                 }
             };
         }
-
         private void StartMultiButton_Click(object sender, RoutedEventArgs e)
         {
             if (_isRunning)
             {
                 _timer.Stop();
                 startMultiButton.Content = "开始抽选";
+
+                foreach (var item in _results)
+                {
+                    if (item != "无更多数据" && item != "无数据")
+                    {
+                        _pickedNames.Add(item);
+                    }
+                }
             }
             else
             {
@@ -91,7 +116,6 @@ namespace RandPicker
             }
             _isRunning = !_isRunning;
         }
-
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             if (Application.Current.MainWindow is MainWindow mainWindow)
