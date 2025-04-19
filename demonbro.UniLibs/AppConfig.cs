64aecfa9fc9f9f4f9e44b152fe584c4646d79d7f
demonbro-dev <kuroama@foxmail.com>
2025-04-19 21:34:24 +0800

    添加初始UniLibs

---
+++ demonbro.UniLibs/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace demonbro.UniLibs
+{
+    public class AppConfig
+    {
+        public double WindowWidth { get; set; } = 800;
+        public double WindowHeight { get; set; } = 600;
+        public string LastUsedList { get; set; } = "default";
+        public string TextColorHex { get; set; } = "#FF000000"; // 使用十六进制格式存储
+        public double AnimationSpeed { get; set; } = 1.0;
+        public bool IsTopMost { get; set; }
+    }
+}
---
+++ demonbro.UniLibs/ConfigurationManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace demonbro.UniLibs
+{
+    public static class ConfigurationManager
+    {
+        private static readonly JsonSerializerOptions Options = new()
+        {
+            WriteIndented = true,
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static AppConfig LoadConfig(string configPath)
+        {
+            try
+            {
+                if (File.Exists(configPath))
+                {
+                    var json = File.ReadAllText(configPath);
+                    return JsonSerializer.Deserialize<AppConfig>(json, Options);
+                }
+            }
+            catch { /* 异常处理 */ }
+
+            return new AppConfig();
+        }
+
+        public static void SaveConfig(AppConfig config, string configPath)
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(config, Options);
+                File.WriteAllText(configPath, json);
+            }
+            catch { /* 异常处理 */ }
+        }
+    }
+}
